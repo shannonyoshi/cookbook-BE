4df2e0f35f4680e29f863108098973e5f3fd848d
ken1286 <kindle1286@gmail.com>
2019-06-25 12:13:55 -0700

    username made

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -29,10 +29,7 @@ router.post('/login', (req, res) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user); // <<<<<<<<<<<<<<<<<<<<<<<<<
 
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-          token, // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-        });
+        res.status(200).json({username: user.username, title: user.title, tagline: user.tagline, token});
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
