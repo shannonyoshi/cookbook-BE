5288602cd3a3cdf3f9605e194b2b27ee18db77c8
Shannon <shannon.yoshikawa@gmail.com>
2020-02-26 10:41:57 -0800

    update directory

--- knexfile.js
+++ knexfile.js
@@ -46,7 +46,7 @@ module.exports = {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
     migratons: {
-      directory: './data/migrations'
+      directory: "/app/data/migrations"
     },
     seeds: {
       directory: './data/seeds'
